a7e6152b5f93f8b2888caed404436fce5951c8c6
ChikaLabita <70810801+ChikaLabita@users.noreply.github.com>
2020-09-09 16:02:03 +0700

    Add files via upload

---
+++ Latihan Teori-PBO/Model/Mahasiswa.java
@@ -0,0 +1,51 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Mahasiswa {
+
+    /**
+     * Default constructor
+     */
+    public Mahasiswa() {
+    }
+
+    /**
+     * 
+     */
+    public String namaMhs;
+
+    /**
+     * 
+     */
+    public int nimMhs;
+
+    /**
+     * @param newValue 
+     * @return
+     */
+    public void setnamaMhs(String newValue) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param newValue 
+     * @return
+     */
+    public void setnimMhs(int newValue) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public void tampilData() {
+        // TODO implement here
+        return null;
+    }
+
+}
---
+++ Latihan Teori-PBO/Model/NilaiMahasiswa.java
@@ -0,0 +1,51 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class NilaiMahasiswa extends Mahasiswa {
+
+    /**
+     * Default constructor
+     */
+    public NilaiMahasiswa() {
+    }
+
+    /**
+     * 
+     */
+    public Int nilaiMhs;
+
+    /**
+     * 
+     */
+    public String matKul;
+
+    /**
+     * @param nilaiMhs 
+     * @return
+     */
+    public void setnilaiMhs(int nilaiMhs) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param newValue 
+     * @return
+     */
+    public void setmatKul(String newValue) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public void tampilData() {
+        // TODO implement here
+        return null;
+    }
+
+}
