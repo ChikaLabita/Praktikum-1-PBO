45fa7b531b74b48c0d2fe2eb712c2ac0c2516029
ChikaLabita <70810801+ChikaLabita@users.noreply.github.com>
2020-09-09 16:53:21 +0700

    Add files via upload

---
+++ Latihan Teori-PBO/Model/Persegi.java
@@ -0,0 +1,55 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Persegi {
+
+    /**
+     * Default constructor
+     */
+    public Persegi() {
+    }
+
+    /**
+     * 
+     */
+    public int sisi;
+
+    /**
+     * @param sisi 
+     * @return
+     */
+    public void dataPersegi(int sisi) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param sisi*sisi 
+     * @return
+     */
+    public void luasPersegi(int sisi*sisi) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param 4*sisi 
+     * @return
+     */
+    public void kelilingPersegi(int 4*sisi) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public void TampilPersegi() {
+        // TODO implement here
+        return null;
+    }
+
+}
