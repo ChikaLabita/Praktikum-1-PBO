c7aa93d986186e1233e495c33c5e4a885a229e12
ChikaLabita <70810801+ChikaLabita@users.noreply.github.com>
2020-11-02 16:16:19 +0700

    Add files via upload

---
+++ Pertemuan 10/src/com/abstractclass/Ikan.java
@@ -0,0 +1,9 @@
+package com.abstractclass;
+
+public class Ikan extends Hewan {
+    @Override
+    public void bergerak(){
+        System.out.println("Berjalan dengan SIRIP, \"wush..wush..\"");
+    }
+}
+
---
+++ Pertemuan 10/src/com/abstractclass/Program.java
@@ -0,0 +1,19 @@
+package com.abstractclass;
+
+public class Program {
+
+    public static void main(String[] args) {
+	// write your code here
+        Kucing kucingKampung = new Kucing();
+        Ikan lumbalumba = new Ikan();
+
+        Orang ani = new Orang("Ani");
+        Orang budi = new Orang("Budi");
+
+        ani.peliharaHewan(kucingKampung);
+        budi.peliharaHewan(lumbalumba);
+
+        ani.ajakPeliharaanJalanJalan();
+        budi.ajakPeliharaanJalanJalan();
+    }
+}
